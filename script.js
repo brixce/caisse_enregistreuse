c2e7c991277e62562dbb0baad19b38e4eed6b08c
brixce <164036929+brixce@users.noreply.github.com>
2024-03-19 16:17:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,34 @@ fetch("items.json")
   .then((data) => {
     for (let item of data.items) {
       let itemElement = document.createElement("div");
-      itemElement.innerHTML = `<span class="name">${item.name}</span> - <span class="price">${item.price}</span>€ <button>Ajouter au panier</button>`;
       itemElement.className = "item";
-      document.getElementById("items").appendChild(itemElement);
+
+      let nameElement = document.createElement("span");
+      nameElement.className = "name";
+      nameElement.textContent = item.name;
+
+      let priceElement = document.createElement("span");
+      priceElement.className = "price";
+      priceElement.textContent = ` ${item.price}€`;
+
+      let imageElement = document.createElement("img");
+      imageElement.src = item.image;
+
+      let buttonElement = document.createElement("button");
+      buttonElement.textContent = "Ajouter au panier";
 
       // Ajoute un gestionnaire d'événements pour le bouton
-      itemElement.querySelector("button").addEventListener("click", () => {
+      buttonElement.addEventListener("click", () => {
         total += item.price;
         document.getElementById("total").textContent = total.toFixed(2);
       });
+
+      itemElement.appendChild(nameElement);
+      itemElement.appendChild(priceElement);
+      itemElement.appendChild(imageElement);
+      itemElement.appendChild(buttonElement);
+
+      document.getElementById("items").appendChild(itemElement);
     }
   })
   .catch((error) => console.error("Erreur:", error));
